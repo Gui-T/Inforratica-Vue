bb86c3bc41ea04d906eb885f6953cf2f8797a425
Guilherme Tamanini <guilherme.gui.tama@gmail.com>
2023-07-06 10:37:07 -0300

    Errors fixed

--- src/api/computadores.js
+++ src/api/computadores.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 export default class ComputadoresApi {
   async buscarTodosOsComputadores() {
-    const { data } = await axios.get('http://192.168.1.14:19003/computadores');
+    const { data } = await axios.get('http://127.0.0.1:8000/computadores');
     return data;
   }
   async adicionarComputador(computador) {
-    const { data } = await axios.post('http://192.168.1.14:19003/computadores', computador);
+    const { data } = await axios.post('http://127.0.0.1:8000/computadores/', computador);
     return data;
   }
   async atualizarComputador(computador) {
-    const { data } = await axios.put(`http://192.168.1.14:19003/computadores/${computador.id}/`, computador);
+    const { data } = await axios.put(`http://127.0.0.1:8000/computadores/${computador.id}/`, computador);
     return data;
   }
   async excluirComputador(id) {
-    const { data } = await axios.delete(`http://192.168.1.14:19003/computadores/${id}/`);
+    const { data } = await axios.delete(`http://127.0.0.1:8000/computadores/${id}/`);
     return data;
   }
 }
